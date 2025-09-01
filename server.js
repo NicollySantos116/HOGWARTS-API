c8df2fe56d5f554e44a64f960e06a5078b6e7710
nicolly <saantos.nick1606@gmail.com>
2025-09-01 08:45:28 -0300

    feat:Adciona post e get com filtro

--- server.js
+++ server.js
@@ -183,6 +183,69 @@ app.get("/bruxos/casa/:casa", (req, res) => {
     }
 });
 
+app.get('/bruxos', (req, res) => {
+  const { casa, ano, especialidade, nome } = req.query;
+  let resultado = bruxos;
+
+  if (casa) {
+      resultado = resultado.filter((b) => b.casa.toLowerCase().includes(casa.toLowerCase()));
+  }
+
+  if (ano) {
+    resultado = resultado.filter(b => b.ano == ano);
+  }
+
+  if (especialidade) {
+    resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
+  }
+
+  if (nome) {
+    resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
+  }
+
+  res.status(200).json({
+    total: resultado.length,
+    data: resultado
+  });
+});
+
+app.post('/bruxos', (req, res) => {
+    // Acessando dados do body
+    const { nome, casa, ano, varinha, mascote, patrono, especialidade, vivo } = req.body;
+    
+    console.log('Dados recebidos:', req.body);
+    
+    // Validação básica
+    if (!nome || !casa) {
+        return res.status(400).json({
+            success: false,
+            message: "Nome e casa são obrigatórios para um bruxo!"
+        });
+    }
+    
+    // Criar novo bruxo
+    const novoBruxo = {
+        id: bruxos.length + 1,
+        nome,
+        casa: casa,
+        ano: parseInt(ano),
+        varinha: varinha,
+        mascote: mascote,
+        patrono: patrono,
+        especialidade: especialidade || "Em desenvolvimento",
+        vivo: vivo
+    };
+    
+    // Adicionar à lista de bruxos
+    bruxos.push(novoBruxo);
+    
+    res.status(201).json({
+        success: true,
+        message: "Novo bruxo adicionado a Hogwarts!",
+        data: novoBruxo
+    });
+});
+
 // Iniciar servidor
 app.listen(serverPort, () => {
   console.log(`⚡ Servidor Hogwarts iniciado em: http://localhost:${serverPort}`);
